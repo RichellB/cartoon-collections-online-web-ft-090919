8462893d17c7608cc9dae4b34991dece8ad3e755
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-15 03:06:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
+  array.each_with_index do |name, num|
+    puts "#{num} #{name}"
+  end
+    
 end
 
 def summon_captain_planet# code an argument here
