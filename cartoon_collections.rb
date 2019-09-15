6b93ffaa1c4aa0c8fbee090476b04f0b703c7cf3
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-15 03:40:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   new_array = []
-  new_array<< array.collect {|element| element.capitalize + "!"}
+  new_array << array.collect {|element| element.capitalize + "!"}
   new_array
 end
 
