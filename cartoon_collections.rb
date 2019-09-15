ecc27e53f93fe528828dcc2661e501981328eeec
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-15 03:40:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,9 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.collect {|element| element.capitalize + "!"}
+  new_array = []
+  new_array<< array.collect {|element| element.capitalize + "!"}
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
